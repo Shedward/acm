9d8d45434ccf4f5aacac39868816b336daf3b4b4
Maltsev Vlad <shedwardx@gmail.com>
2013-10-11 00:08:33 +0700

    Using g++ without c++11 standart when it posible.

--- 1197.Lonesome_Knight.cpp
+++ 1197.Lonesome_Knight.cpp
@@ -2,7 +2,7 @@
  // Problem: 1197. Lonesome Knight
  // Url: http://acm.timus.ru/problem.aspx?space=1&num=1197
  // Author: Shed (86286AA)
- // Language: g++11 (G++ 4.7.2 with C++11)
+ // Language: g++ (G++ 4.7.2 with C++11)
  // Tests: 1197.Lonesome_Knight.tests
 
 #include <stdio.h>
--- 1264.Workdays.cpp
+++ 1264.Workdays.cpp
@@ -2,7 +2,7 @@
  // Problem: 1264. Workdays
  // Url: http://acm.timus.ru/problem.aspx?space=1&num=1264
  // Author: Shed (86286AA)
- // Language: g++11 (G++ 4.7.2 with C++11)
+ // Language: g++ (G++ 4.7.2 with C++11)
  // Tests: 1264.Workdays.tests
 
 #include <stdio.h>
