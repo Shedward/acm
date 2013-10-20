09c84cf6f7880058bee9bc164c34cfcacf551d75
Maltsev Vlad <shedwardx@gmail.com>
2013-10-20 19:17:10 +0700

    use stdio for 1196 solution

--- 1196.History_Exam.cpp
+++ 1196.History_Exam.cpp
@@ -5,38 +5,38 @@
  // Language: g++11 (G++ 4.7.2 with C++11)
  // Tests: 1196.History_Exam.tests
 
-#include <iostream>
+#include <stdio.h>
 #include <set>
 
 int main()
 {
 	int n;
-	std::cin >> n;
+	scanf("%u", &n);
 
 	std::set<int> pdates;
 	for (int i = 0; i < n; ++i)
 	{
 		int date;
-		std::cin >> date;
+		scanf("%u", &date);
 		pdates.insert(date);
 	}
 
 	int res = 0;
 
 	int m;
-	std::cin >> m;
+	scanf("%u", &m);
 
 	for (int i = 0; i < m; ++i)
 	{
 		int date;
-		std::cin >> date;
+		scanf("%u", &date);
 
 		if (pdates.count(date)){
 			++res;
 		}
 	}
 
-	std::cout << res;
+	printf("%u\n", res);
 	return 0;
 }
 
