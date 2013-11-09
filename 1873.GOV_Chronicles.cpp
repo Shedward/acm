585172a239568d98e3f350a698a4b4e241759f14
Maltsev Vlad <shedwardx@gmail.com>
2013-11-09 22:09:28 +0700

    1873 solution.

---
+++ 1873.GOV_Chronicles.cpp
@@ -0,0 +1,15 @@
+
+ // Problem: 1873. GOV Chronicles
+ // Url: http://acm.timus.ru/problem.aspx?space=1&num=1873
+ // Author: Shed (86286AA)
+ // Language: g++11 (G++ 4.7.2 with C++11)
+ // Tests: 1873.GOV_Chronicles.tests
+
+#include <iostream>
+
+int main() {
+	int n, p[]={5,20,12,2,1,4,6,1,4,4,1,0};
+    std::cin >> n;
+    std::cout << p[n];
+    return 0;
+}
